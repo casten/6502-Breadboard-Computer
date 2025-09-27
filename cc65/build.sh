41baf772b58009bff7a0dda72f7ac585efd2af40
Casten Riepling <casten.riepling@gmail.com>
2025-09-27 15:37:33 -0700

    build script updates
    Change build script to match readme.

--- cc65/build.sh
+++ cc65/build.sh
@@ -11,6 +11,6 @@ ca65 --cpu 65sc02 wait.s
 ca65 --cpu 65sc02 lcd.s
 
 echo "Linking..."
-ld65 -C breadboard.cfg -m hello.map interrupt.o vectors.o wait.o lcd.o hello.o sbc.lib
+ld65 -C breadboard.cfg -m hello.map interrupt.o vectors.o wait.o lcd.o hello.o none.lib
 
 echo "Done."
